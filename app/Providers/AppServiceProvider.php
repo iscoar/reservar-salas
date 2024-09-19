4d7f7d94c62bd045849f8a85e2f702e24f77ceeb
Francisco Amador <isco.amaador@gmail.com>
2024-09-18 18:41:35 -0600

    feat: add repositories and responders for dependency injection

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Src\Booking\Domain\{BookingRepositoryInterface, BookingRepository};
+use Src\Common\Responders\{ErrorResponderInterface, ErrorResponder};
+use Src\Common\Responders\{SuccessResponderInterface, SuccessResponder};
+use Src\MeetingRoom\Domain\{MeetingRoomRepositoryInterface, MeetingRoomRepository};
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +17,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            ErrorResponderInterface::class,
+            ErrorResponder::class
+        );
+        $this->app->bind(
+            SuccessResponderInterface::class,
+            SuccessResponder::class
+        );
+        $this->app->bind(
+            MeetingRoomRepositoryInterface::class,
+            MeetingRoomRepository::class
+        );
+        $this->app->bind(
+            BookingRepositoryInterface::class,
+            BookingRepository::class
+        );
     }
 
     /**
