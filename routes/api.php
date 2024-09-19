68723915ac0af1007bd61c234aaee1812a223516
Francisco Amador <isco.amaador@gmail.com>
2024-09-18 18:42:14 -0600

    feat: add all routes for meeting rooms crud and create/cancel bookings

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\{Route, DB};
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +13,12 @@ use Illuminate\Support\Facades\{Route, DB};
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Rutas para el CRUD de las salas de junta
+Route::get('/meeting-rooms', 'MeetingRoom\Actions\ShowAllMeetingRooms');
+Route::post('/meeting-room', 'MeetingRoom\Actions\CreateMeetingRoom');
+Route::put('/meeting-room/{room_id}', 'MeetingRoom\Actions\EditMeetingRoom');
+Route::delete('/meeting-room/{room_id}', 'MeetingRoom\Actions\DeleteMeetingRoom');
+
+// Rutas para las reservas de las salas de junta
+Route::post('/meeting-room/{room_id}/booking', 'Booking\Actions\CreateBooking');
+Route::put('/meeting-room/{room_id}/booking/cancel', 'Booking\Actions\CancelBooking');
